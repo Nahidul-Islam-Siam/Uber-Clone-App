e27b855ccb369f687eb7f6b7ce34e9597640f992
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-07-30 06:31:12 +0600

    feat: add node-addon-api conversion tool and node-gyp-build package
    - Introduced a new conversion script for migrating from Nan to Napi in native addons.
    - Added LICENSE, README.md, SECURITY.md, and other necessary files for the node-gyp-build package.
    - Implemented the core functionality for building and loading native modules with prebuild support.
    - Included scripts for testing and optional native dependencies handling.
    - Updated package.json with relevant metadata and dependencies.

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectToDB = require("./db/db");
+const userRouters = require("./routes/user.routes");
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use('/users',userRouters);
+connectToDB();
 
 module.exports = app;
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { body} =  require("express-validator");
+const { body } = require("express-validator");
 const { route } = require("../app");
 
-
-
+const userController = require("../controllers/user.controller");
 
 router.post("/register", [
-body("email").isEmail().withMessage('Invalid email'),
-body("password").isLength({min: 5}).withMessage('Password must be at least 5 characters long'),
-body('')    
-])
+  body("email").isEmail().withMessage("Invalid email"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body(""),
+],
+userController.registerUser
+);
 module.exports = router;
